cda34e0bd0816cc7cdaf3c2ef99cf52eefb54f56
qiaoyanqing <a2901291002@163.com>
2022-02-16 15:41:25 +0800

    监听模式

--- observer/src/subject/ObServer.java
+++ observer/src/subject/ObServer.java
@@ -2,8 +2,16 @@ package subject;
 
 import core.Event;
 
+/**
+ * 监听者
+ */
 public class ObServer {
 
+    /**
+     * 监听者进行通知
+     *
+     * @param event 所通知的事件
+     */
     public void advice(Event event) {
         System.out.println("=========监听========" + event);
     }
